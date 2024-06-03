c666cee221d51ccef0e046fd02c3cfab24013640
Julien Ravilly <julien.ravilly@etudiant.univ-lr.fr>
2024-06-03 15:19:30 +0200

    Implementation of Bullet class

--- CentipedeReloaded/bullet.cpp
+++ CentipedeReloaded/bullet.cpp
@@ -1,3 +1,23 @@
 #include "bullet.h"
 
-Bullet::Bullet() {}
+Bullet::Bullet()
+    :itsHitBox(0, 0, 0, 0), itsPosition({0, 0})
+{ }
+
+Bullet::~Bullet()
+{ }
+
+void Bullet::updatePos()
+{
+    itsPosition.posY = itsPosition.posY - 0; // !-- Change 0 to speed const --!
+}
+
+QRect Bullet::getItsHitBox()
+{
+    return itsHitBox;
+}
+
+Position Bullet::getItsPosition()
+{
+    return itsPosition;
+}
