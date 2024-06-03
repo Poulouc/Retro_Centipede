e99453872eb355c7664fb074b4f3adaa1dffe81c
Quentin Rainaud <quentin.rainaud@etudiant.univ-lr.fr>
2024-06-03 16:42:38 +0200

    centipède à tester

--- CentipedeReloaded/centipede.h
+++ CentipedeReloaded/centipede.h
@@ -8,12 +8,15 @@ class Centipede
 {
 private:
     BodyPart* itsHead;
+    BodyPart* itsTail;
     Direction itsDirection;
 public:
     Centipede(BodyPart* head = new BodyPart());
     ~Centipede();
     void moveForward();
     BodyPart* getItsHead();
+    BodyPart* getItsTail();
+    void setItsTail(BodyPart* tail);
 };
 
 #endif // CENTIPEDE_H
